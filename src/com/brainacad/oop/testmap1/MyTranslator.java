6ee4d973bf7082ce1bfedb8ad4bdf6f9c6c1f637
JohnySta04 <mcmxtrainer@gmail.com>
2018-02-05 19:41:02 +0200

    Labwork 2.15.6-1

--- src/com/brainacad/oop/testmap1/MyTranslator.java
+++ src/com/brainacad/oop/testmap1/MyTranslator.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 
 public class MyTranslator {
 
-    private HashMap<String, String> stringHashMap = new HashMap<>();
+    private HashMap<String, String> dictionary = new HashMap<>();
 
     public void addNewWord(String en, String ru) {
 
-        stringHashMap.put(en, ru);
+        dictionary.put(en, ru);
     }
 
     public String translate (String en) {
@@ -16,7 +16,7 @@ public class MyTranslator {
         StringBuffer result= new StringBuffer();
         String [] strings = en.split(" ");
         for (int i=0; i<strings.length; i++ ) {
-            result.append(stringHashMap.get(strings[i])+' ');
+            result.append(dictionary.get(strings[i])+' ');
         }
         return result.toString();
     }
